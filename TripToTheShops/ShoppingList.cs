d7a67fdd7efc077eb4d4889dba2e1b1087a77271
Дмитрий Ключников <klyuchnikovds@gmail.com>
2012-11-01 12:44:56 +0400

    add new entity ShoppingList

---
+++ TripToTheShops/ShoppingList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace TripToTheShops
+{
+   public class ShoppingList
+    {
+       /// <summary>
+       /// Начальная точка (местоположение покупателя)
+       /// </summary>
+       public Point Coordinates { get; private set; }
+
+       /// <summary>
+       /// Критерий оптимизации
+       /// </summary>
+       public ParameterShopping OptimizeParameter { get; private set; }
+
+       /// <summary>
+       /// Коды товаров для покупки
+       /// </summary>
+       public String[] CodesProduct { get; private set; }
+
+
+       public ShoppingList(Point coordinates, ParameterShopping optimizeParameter, IEnumerable<string> codesProduct)
+       {
+           this.Coordinates = coordinates;
+           this.OptimizeParameter = optimizeParameter;
+           this.CodesProduct = codesProduct.ToArray();
+       }
+
+    }
+}
