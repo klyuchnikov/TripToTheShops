96a8ffd1cbc42b06acf6a95c73ebbb1f0bf4c320
Дмитрий Ключников <klyuchnikovds@gmail.com>
2012-11-01 11:12:48 +0400

    add entity Product

---
+++ TripToTheShops/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TripToTheShops
+{
+    /// <summary>
+    /// Продукт, доступный в магазине
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Код товара
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Цена товара
+        /// </summary>
+        public float Price { get; set; }
+
+        /// <summary>
+        /// Наименование товара
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
