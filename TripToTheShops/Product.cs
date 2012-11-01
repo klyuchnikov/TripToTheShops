1dc4b98032a5a1c66081dcdeb715629cc8043dc4
Дмитрий Ключников <klyuchnikovds@gmail.com>
2012-11-01 11:25:58 +0400

    add constructor Product

--- TripToTheShops/Product.cs
+++ TripToTheShops/Product.cs
@@ -13,16 +13,29 @@ namespace TripToTheShops
         /// <summary>
         /// Код товара
         /// </summary>
-        public string Code { get; set; }
+        public string Code { get; private set; }
 
         /// <summary>
         /// Цена товара
         /// </summary>
-        public float Price { get; set; }
+        public float Price { get; private set; }
 
         /// <summary>
         /// Наименование товара
         /// </summary>
-        public string Name { get; set; }
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Создание нового экземпляра товара
+        /// </summary>
+        /// <param name="code">Код</param>
+        /// <param name="price">Цена</param>
+        /// <param name="name">Наименование</param>
+        public Product(string code, float price, string name)
+        {
+            this.Code = code;
+            this.Price = price;
+            this.Name = name;
+        }
     }
 }
