c46975832616b4628a8340e0f78d9f71703d99e0
Дмитрий Ключников <klyuchnikovds@gmail.com>
2012-11-01 12:09:08 +0400

    add new entity Model

---
+++ TripToTheShops/Model.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Windows;
+
+namespace TripToTheShops
+{
+    public class Model
+    {
+        /// <summary>
+        /// Закрытый конструктор
+        /// </summary>
+        protected Model() { }
+
+        /// <summary>
+        /// Внутреннее поле сущности Модели
+        /// </summary>
+        private static Model current = new Model();
+
+        /// <summary>
+        /// Получение текущей модели
+        /// </summary>
+        public static Model Current { get { return current; } }
+
+        /// <summary>
+        /// Список магазинов (private)
+        /// </summary>
+        private List<Shop> shopsList = new List<Shop>();
+
+        /// <summary>
+        /// Список магазинов
+        /// </summary>
+        public Shop[] Shops { get { return shopsList.ToArray(); } }
+
+
+        public bool IsLoadShops { get; private set; }
+        /// <summary>
+        /// Загрузка магазинов
+        /// </summary>
+        /// <param name="doc">XML документ</param>
+        /// <returns></returns>
+        public bool LoadShops(XDocument doc)
+        {
+            try
+            {
+                var shopsXML = doc.Root.Elements("shop");
+                var shops = new List<Shop>();
+                foreach (var shopXML in shopsXML)
+                {
+                    var id = shopXML.Attribute("id").Value;
+                    var nameShop = shopXML.Element("name").Value;
+                    var cootdinatesXML = shopXML.Element("coordinates");
+                    var cootdinates = new Point(double.Parse(cootdinatesXML.Attribute("x").Value.Replace('.', ',')), double.Parse(cootdinatesXML.Attribute("y").Value.Replace('.', ',')));
+                    var productsXML = shopXML.Element("products").Elements("product");
+                    var products = new List<Product>();
+                    foreach (var productXML in productsXML)
+                    {
+                        var code = productXML.Attribute("code").Value;
+                        var price = float.Parse(productXML.Attribute("price").Value.Replace('.', ','));
+                        var name = productXML.Value;
+                        var product = new Product(code, price, name);
+                        products.Add(product);
+                    }
+                    var shop = new Shop(id, nameShop, cootdinates, products);
+                    shops.Add(shop);
+                }
+                shopsList = shops;
+                IsLoadShops = true;
+                return true;
+            }
+            catch
+            {
+                IsLoadShops = false;
+                return false;
+            }
+        }
+    }
+}
