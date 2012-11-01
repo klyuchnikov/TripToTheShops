dc2ae8eaac28acc5b14e3035feb5ecd381fe017b
Дмитрий Ключников <klyuchnikovds@gmail.com>
2012-11-01 22:19:23 +0400

    modify paint icon shop and GUI controls

--- TripToTheShops/MainWindow.xaml.cs
+++ TripToTheShops/MainWindow.xaml.cs
@@ -24,23 +24,7 @@ namespace TripToTheShops
         {
             InitializeComponent();
             globalGrid.DataContext = Model.Current;
-            var el = new Ellipse() { Height = 30, Width = 30 };
-            el.Fill = Brushes.Red;
-            //     Canvas.SetLeft(el, 0);
-            //     Canvas.SetTop(el, 0);
-            //    canvas1.Children.Add(el);
-
-            canvas1.UpdateLayout();
-            Image image = new Image
-            {
-                Width = 48,
-                Source = new BitmapImage(new Uri(@"/TripToTheShops;component/magaz.png", UriKind.Relative))//,
-               // Margin = new Thickness(100,0,0,0)
-            };
-            Canvas.SetLeft(image, 500);
-            canvas1.Children.Add(image);
-            canvas1.Width = 500 + 200;
-            canvas1.UpdateLayout();
+            PaintShopsToCanvas();
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
@@ -52,9 +36,51 @@ namespace TripToTheShops
             globalGrid.DataContext = null;
             Model.Current.LoadShops(OFD.FileName);
             globalGrid.DataContext = Model.Current;
-
             globalGrid.UpdateLayout();
+            PaintShopsToCanvas();
 
+        }
+
+        private void PaintShopsToCanvas()
+        {
+            canvas1.Width = 0;
+            canvas1.Height = 0;
+            canvas1.Children.Clear();
+            if (Model.Current.Shops.Length == 0)
+                return;
+            var minX = Model.Current.Shops.Select(q => q.Coordinates.X).Min();
+            var minY = Model.Current.Shops.Select(q => q.Coordinates.Y).Min();
+            var min = minX < minY ? minX : minY;
+            min = Math.Abs(min) + 100;
+            foreach (var shop in Model.Current.Shops)
+            {
+                Image image = new Image
+                {
+                    Width = 48,
+                    Source = new BitmapImage(new Uri(@"/TripToTheShops;component/magaz.png", UriKind.Relative))
+                };
+                Canvas.SetLeft(image, shop.Coordinates.X + min);
+                Canvas.SetTop(image, shop.Coordinates.Y + min);
+                canvas1.Children.Add(image);
+                Label labelName = new Label() { Content = shop.Name };
+                Canvas.SetLeft(labelName, shop.Coordinates.X + min + 50);
+                Canvas.SetTop(labelName, shop.Coordinates.Y + min);
+                canvas1.Children.Add(labelName);
+                if (shop.Coordinates.X + min + 100 > canvas1.Width)
+                    canvas1.Width = shop.Coordinates.X + min + 100;
+                if (shop.Coordinates.Y + min + 100 > canvas1.Height)
+                    canvas1.Height = shop.Coordinates.Y + min + 100;
+            }
+            // set icon home
+            Image imageHome = new Image
+            {
+                Width = 48,
+                Source = new BitmapImage(new Uri(@"/TripToTheShops;component/kfm_home.png", UriKind.Relative))
+            };
+            Canvas.SetLeft(imageHome, min);
+            Canvas.SetTop(imageHome, min);
+            canvas1.Children.Add(imageHome);
+            canvas1.UpdateLayout();
         }
     }
 }
