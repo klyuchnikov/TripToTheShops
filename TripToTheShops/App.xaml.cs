694c73673dc62dcbecc5064dc38247bc83014972
Дмитрий Ключников <klyuchnikovds@gmail.com>
2012-11-01 11:07:13 +0400

    add startup code

--- TripToTheShops/App.xaml.cs
+++ TripToTheShops/App.xaml.cs
@@ -12,5 +12,9 @@ namespace TripToTheShops
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            
+        }
     }
 }
