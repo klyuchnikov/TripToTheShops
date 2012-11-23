6e7882aa00c353bed8ae099034a650ff576b8404
Дмитрий Ключников <klyuchnikovds@gmail.com>
2012-11-24 03:30:44 +0400

    final

---
+++ src - копия/TripToTheShops/Product.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TripToTheShops
+{
+    /// <summary>
+    /// Продукт, доступный в магазине
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Код товара
+        /// </summary>
+        public string Code { get; private set; }
+
+        /// <summary>
+        /// Цена товара
+        /// </summary>
+        public float Price { get; private set; }
+
+        /// <summary>
+        /// Наименование товара
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Создание нового экземпляра товара
+        /// </summary>
+        /// <param name="code">Код</param>
+        /// <param name="price">Цена</param>
+        /// <param name="name">Наименование</param>
+        public Product(string code, float price, string name)
+        {
+            this.Code = code;
+            this.Price = price;
+            this.Name = name;
+        }
+
+        /// <summary>
+        /// В каком магазине лежит
+        /// </summary>
+        public Shop Shop { get; private set; }
+
+        /// <summary>
+        /// Присвоить значение магазина
+        /// </summary>
+        /// <param name="shop"></param>
+        public void SetShop(Shop shop)
+        {
+            this.Shop = shop;
+        }
+    }
+
+    // Custom comparer for the Product class
+    class ProductComparer : IEqualityComparer<Product>
+    {
+        // Products are equal if their names and product numbers are equal.
+        public bool Equals(Product x, Product y)
+        {
+            //Check whether the products' properties are equal.
+            return x.Code == y.Code;
+        }
+
+        // If Equals() returns true for a pair of objects 
+        // then GetHashCode() must return the same value for these objects.
+
+        public int GetHashCode(Product product)
+        {
+            //Check whether the object is null
+            if (Object.ReferenceEquals(product, null)) return 0;
+
+            //Get hash code for the Code field.
+            int hashProductCode = product.Code.GetHashCode();
+
+            //Calculate the hash code for the product.
+            return hashProductCode;
+        }
+
+    }
+
+}
---
+++ src - копия/TripToTheShops/Shop.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace TripToTheShops
+{
+    /// <summary>
+    /// Сущность "Магазин"
+    /// </summary>
+    public class Shop
+    {
+        /// <summary>
+        /// Идентификатор магазина
+        /// </summary>
+        public string ID { get; private set; }
+
+        /// <summary>
+        /// Название магазина 
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Координаты магазина
+        /// </summary>
+        public Point Coordinates { get; private set; }
+
+        /// <summary>
+        /// Список продаваемых продуктов
+        /// </summary>
+        public Product[] Products { get; private set; }
+
+        /// <summary>
+        /// Создание нового экземпляра магазина
+        /// </summary>
+        /// <param name="id">Идентификатор</param>
+        /// <param name="name">Название</param>
+        /// <param name="coordinates">Координаты</param>
+        /// <param name="products">Список продуктов</param>
+        public Shop(string id, string name, Point coordinates, IEnumerable<Product> products)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Coordinates = coordinates;
+            this.Products = products.ToArray();
+            foreach (var p in this.Products)
+                p.SetShop(this);
+        }
+    }
+}
